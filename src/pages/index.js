6735c3e7a3695a78de5ee293943a6949b4814c6e
Johannes Rupieper (D056754) <Johannes.Rupieper@sap.com>
2023-10-30 09:59:53 +0100

    latest page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
@@ -7,34 +7,60 @@ import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
 import styles from './index.module.css';
 
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-}
-
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+  const [offset, setOffset] = useState(0);
+
+  useEffect(() => {
+      const onScroll = () => setOffset(window.pageYOffset);
+      // clean up code
+      window.removeEventListener('scroll', onScroll);
+      window.addEventListener('scroll', onScroll, { passive: true });
+      return () => window.removeEventListener('scroll', onScroll);
+  }, []);
+
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
+      <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container landingpage-intro">
+        <h1 style={{marginTop:(offset * (-0.2))}} className="hero__title">{siteConfig.title}</h1>
+        <p style={{marginTop:(offset * (-0.3))}} className="hero__subtitle">{siteConfig.tagline}</p>
+        <img style={{marginTop:(offset * (-0.4))}} className="landingpage-screenshot" src={require('@site/static/img/landingpage_screenshot.png').default}></img>
+      </div>
+    </header>
       <main>
-        <HomepageFeatures />
+        <div className='intro-main white'>
+          <Link
+            className="button button--primary button--lg intro-getstarted-button"
+            to="/docs/get-started">
+            Get started
+          </Link>
+        </div>
+        <div className='intro-container white'>
+          <div className='intro-item-txt align-left'>
+            <h1 className='rozha-font'>import & merge</h1>
+            <p>Add files via drag-n-drop to  ~/.kube/config file.</p>
+          </div>
+          <img className='intro-item-img' src={require('@site/static/img/landingpage_merge.png').default}></img>
+        </div>
+
+        <div className='intro-container white'>
+          <img className='intro-item-img' src={require('@site/static/img/landingpage-organized.png').default}></img>
+          <div className='intro-item-txt align-right'>
+            <h1 className='rozha-font'>stay organized</h1>
+            <p>categorise and mark your favorite contexts.</p>
+          </div>
+        </div>
+
+        <div className='intro-container white'>
+          <div className='intro-item-txt align-left'>
+            <h1 className='rozha-font'>switch in seconds</h1>
+            <p>Use the MenuBar app and switch contexts anytime.</p>
+          </div>
+          <img className='intro-item-img' src={require('@site/static/img/landingpage-menubar.png').default}></img>
+        </div>
       </main>
     </Layout>
   );
